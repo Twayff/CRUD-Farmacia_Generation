3aad084216e0d241b9dc63a09631f84a31683cf6
Twayff <thiagolacerda.gtl@hotmail.com>
2024-03-26 14:28:45 -0300

    Configurando-o-Projeto

---
+++ src/main/java/com/generation/farmaciagtl/FarmaciaGtlApplication.java
@@ -0,0 +1,13 @@
+package com.generation.farmaciagtl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciaGtlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciaGtlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/farmaciagtl/FarmaciaGtlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.farmaciagtl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaGtlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
